8c5698d61e2bf8c5481f7ef898226980890951f1
NGAN <NGAN@DESKTOP-M5ENFJV>
2022-03-03 21:16:55 +0700

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port=3000
+
+app.get('/', function (req, res) {
+    var a=1;
+    var b=1;
+    var c=a+b;
+  res.send('Hello World')
+})
+
+app.listen(port,()=> console.log(`Example app listening at http://localhost:${port}`))
